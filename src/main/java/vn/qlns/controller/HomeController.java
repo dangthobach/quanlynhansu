108b995d0f347d6ad0f9ff52c2f5a87e57e6330f
Đặng Thọ Bách <dangthobach@gmail.com>
2018-12-27 13:29:26 +0700

    update

--- src/main/java/vn/qlns/controller/HomeController.java
+++ src/main/java/vn/qlns/controller/HomeController.java
@@ -9,8 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import vn.qlns.model.Bu;
 import vn.qlns.model.Staff;
@@ -26,11 +29,12 @@ public class HomeController {
 	@Autowired
 	StaffService staffService;
 	
+
 	@RequestMapping("/")
 	public String listAll(Model model) {
 		
 		List<StaffDTO> list= buService.listAll();
-		model.addAttribute("list", list);
+		model.addAttribute("staffList", list);
 		return "home";
 	}
 	
@@ -43,11 +47,12 @@ public class HomeController {
 	
 	@RequestMapping(value = "/add", method = RequestMethod.POST)
 	public String addStaff(@ModelAttribute("staff") Staff staff, HttpServletRequest request) {
+		
 		staffService.save(staff);
 		return "addStaff";
 	}
 	
-	
+
 	
 	
 }
